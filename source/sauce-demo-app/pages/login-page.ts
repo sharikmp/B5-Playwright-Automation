c023bdd8e3bfb616e1f904eb7f901463ff9bc2a8
Sharik <sharik@MacBook-Air-Sharik.local>
2025-11-20 00:16:59 +0530

    POM

---
+++ source/sauce-demo-app/pages/login-page.ts
@@ -0,0 +1,28 @@
+import {Page} from "@playwright/test";
+
+export class LoginPage {
+
+    private page: Page;
+
+    constructor(page: Page) {
+        this.page = page;   
+    }
+
+    private locators = {
+        usernameSelector: "#user-name",
+        passwordSelector: "#pwd",
+        loginButtonSelector: "#login-button",
+        errorText: '[data-test="error"]',
+    }
+
+    async performLogin(username: string, password: string) {
+        await this.page.locator(this.locators.usernameSelector).fill(username);
+        await this.page.locator(this.locators.passwordSelector).fill(password);
+        await this.page.locator(this.locators.loginButtonSelector).click();
+    }
+
+    async getError() {
+        return await this.page.locator(this.locators.errorText).textContent();
+    }
+
+}
